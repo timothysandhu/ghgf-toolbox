cf4a9f271a19f09652e1cd18aa73b0aedabfd259
Tim Sandhu <ts772@cam.ac.uk>
2026-07-30 16:15:08 +0100

    refactor find_param_fields

--- funcs/find_param_fields.m
+++ funcs/find_param_fields.m
@@ -1,5 +1,6 @@
 function [param_names,param_trans] = find_param_fields(c)
-%find_param_fields
+%FIND_PARAM_FIELDS Extract parameter names and transforms from an hgf
+%   prc config struct with paired *mu / *sa fields.
 
 %% input check 
 
@@ -12,11 +13,12 @@ end
 % get field names 
 f_names = fieldnames(c);
 
-% which end in mu
-[mu_mask,mu_strip] = match_end_stripped(f_names,"mu");
+% which end in mu / sa
+mu_mask = endsWith(f_names, "mu");
+sa_mask = endsWith(f_names, "sa");
 
-% which end in sa
-[sa_mask,sa_strip] = match_end_stripped(f_names,"sa");
+mu_strip = regexprep(f_names(mu_mask), "mu$", "");
+sa_strip = regexprep(f_names(sa_mask), "sa$", "");
 
 % check the stripped values match
 if ~all(strcmp(sa_strip,mu_strip))
@@ -24,92 +26,56 @@ if ~all(strcmp(sa_strip,mu_strip))
 end
 
 % check the positions match 
-if find(mu_mask) ~= (find(sa_mask)-1)
+if ~isequal(find(mu_mask), find(sa_mask) - 1)
     error("the mu and sa are mismatched")
 end
 
-% check for transformations
-
-% return the parameters if all good
+% check for transformations, return the parameters if all good
 [param_names,param_trans] = check_param_trans(mu_strip);
 
 end
-%% functions 
 
-function [mask,stripped] = match_end_stripped(f_names,end_str)
-if class(f_names) ~= "cell"
-    error("supply the cell array of field names")
-end
+%% functions
+function stripped = strip_start(param_names, start_str)
 
-% length of str 
-end_len = strlength(end_str);
-
-% mask
-mask = endsWith(string(f_names), end_str);
-
-% matches 
-matches = f_names(mask);
-
-% strip the end
-strip_end = @(x) x(1:end-(end_len));
-stripped = cellfun(strip_end,matches,'UniformOutput',false);
-
-end
-
-function [stripped] = strip_start(param_names,start_str)
-% check stripped param names 
 check_stripped_param_names(param_names);
 
-% n_params 
-n_params = length(param_names);
+names = string(param_names);
+mask = startsWith(names, start_str);
 
-% length of str 
-start_len = strlength(start_str);
+stripped = names;
+stripped(mask) = extractAfter(names(mask), start_str);
 
-% mask
-mask = startsWith(string(param_names), start_str);
-
-% strip the start
-stripped = param_names;
-strip_start = @(x) x(1+start_len:end);
-
-for i = 1:n_params
-    if mask(i)
-        stripped{i} = strip_start(stripped{i});
-    end
-end
-
+stripped = cellstr(stripped);
 end
 
 function [out_param_names,ptrans] = check_param_trans(in_param_names)
-% check stripped param names 
 check_stripped_param_names(in_param_names);
 
-% initialize output 
-ptrans = cell(length(in_param_names),1);
+names = string(in_param_names);
+n = numel(names);
 
-% possible transformations are log/logit 
-mask_logit = startsWith(string(in_param_names), "logit");
-mask_log   = startsWith(string(in_param_names), "log") & ~mask_logit;
+% possible transformations are log/logit
+mask_logit = startsWith(names, "logit");
+mask_log   = startsWith(names, "log") & ~mask_logit;
 
-% label 
+ptrans = cell(n,1);
 ptrans(mask_logit) = {"logit"};
-ptrans(mask_log) = {"log"};
+ptrans(mask_log)   = {"log"};
 
-% strip 
-new_param_names = strip_start(in_param_names,"logit");
-out_param_names = strip_start(new_param_names,"log");
-
+% strip
+new_param_names = strip_start(names, "logit");
+out_param_names = strip_start(new_param_names, "log");
 end
 
 function check_stripped_param_names(param_names)
-% should be cell array
-if class(param_names) ~= "cell"
+
+if ~iscell(param_names)
     error("supply the cell array of stripped param names")
 end
 
-% check stripping 
-if any(cellfun(@(x) endsWith(x,"mu"),param_names))||any(cellfun(@(x) endsWith(x,"sa"),param_names))
+names = string(param_names);
+if any(endsWith(names, "mu")) || any(endsWith(names, "sa"))
     error("supply the cell array of stripped param names")
 end
 end
